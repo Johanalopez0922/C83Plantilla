585ea44526e15f84cacae9f1eb1f692bc76e7f75
Johanalopez0922 <johasaron@gmail.com>
2023-09-26 14:09:49 -0500

    Styles

--- Screens/Search.js
+++ Screens/Search.js
@@ -11,18 +11,18 @@ export default class SearchScreen extends Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#5653D4",
-  },
-  text: {
-    color: "#ffff",
-    fontSize: 30,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center",
+//     backgroundColor: "#5653D4",
+//   },
+//   text: {
+//     color: "#ffff",
+//     fontSize: 30,
+//   },
+// });
 
 const styles = StyleSheet.create({
   container: {
